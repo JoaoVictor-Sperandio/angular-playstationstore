588138f47e91b3f0dfcb8f381599923e617bf064
Joao Victor Alonso de Paula Sperandio <joaovictorapsperandio@gmail.com>
2023-09-29 21:46:44 -0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule implements OnInit  {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+ }
